35fc21a840a79c5e74e4897f05ebe349e54543fa
mausLe <anhlt.cse@gmail.com>
2021-04-16 04:03:15 +0700

    Add Color to listview

--- app/src/main/java/com/google/mlkit/vision/demo/kotlin/WatchList.kt
+++ app/src/main/java/com/google/mlkit/vision/demo/kotlin/WatchList.kt
@@ -3,6 +3,6 @@ package com.google.mlkit.vision.demo.kotlin
 import android.graphics.Bitmap
 
 
-data class WatchList(var watchlistID: Int, var imageCode: Bitmap?, var name: String, var time: String) {
+data class WatchList(var watchlistID: Int, var imageCode: Bitmap?, var name: String, var time: String, var type : String) {
 
 }
